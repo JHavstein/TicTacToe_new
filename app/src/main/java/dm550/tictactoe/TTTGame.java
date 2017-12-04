4e118fc7ee62322a1ad6467885d2b3276de3f2de
Jonas Havstein Eriksen <jonas.havstein@gmail.com>
2017-12-04 12:51:10 +0100

    Small change to checkResult() in TTTGame

--- app/src/main/java/dm550/tictactoe/TTTGame.java
+++ app/src/main/java/dm550/tictactoe/TTTGame.java
@@ -65,7 +65,7 @@ public class TTTGame implements Game {
         if (winner > 0) {
             this.ui.showResult("Player "+winner+" wins!");
         }
-        if (this.board.checkFull()) {
+        else if (this.board.checkFull()) {
             this.ui.showResult("This is a DRAW!");
         }
     }
